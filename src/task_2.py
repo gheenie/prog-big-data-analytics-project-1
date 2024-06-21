93e4ee70be5234cbc03c6d296544ba6705c3a323
infoomics <sbx_94@hotmail.com>
2024-06-21 13:34:32 +0100

    Strip lines and migrate file path logic

--- src/task_2.py
+++ src/task_2.py
@@ -1,5 +1,17 @@
-def extract_urls(file_path):
-    with open(file_path, 'r') as file:
+import os
+
+
+def extract_urls():
+    output_folder = "data"
+    output_filename = "video_urls.txt"
+    output_path = os.path.join(output_folder, output_filename)
+
+    with open(output_path, 'r') as file:
         urls = file.readlines()
 
-        return urls
+    for i, url in enumerate(urls):
+        urls[i] = url.strip()
+        
+    print(urls)
+
+    return urls
