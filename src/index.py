987c60eb7b248135f8f2535c385858faa89540d7
infoomics <sbx_94@hotmail.com>
2024-06-21 01:24:13 +0100

    Split __main__ to index.py

---
+++ src/index.py
@@ -0,0 +1,6 @@
+from task_2 import extract_urls
+
+
+if __name__ == '__main__':
+    urls = extract_urls('data/video_urls.txt')
+    print(urls)
--- src/task_2.py
+++ src/task_2.py
@@ -3,8 +3,3 @@ def extract_urls(file_path):
         urls = file.readlines()
 
         return urls
-
-
-if __name__ == '__main__':
-    urls = extract_urls('data/video_urls.txt')
-    print(urls)
