9a90c7bac353183d0f6957820e3a3d670dfb6c34
infoomics <sbx_94@hotmail.com>
2024-06-21 13:34:42 +0100

    Migrate file path logic

--- src/index.py
+++ src/index.py
@@ -4,8 +4,7 @@ from task_3 import serial_download, parallel_download
 
 
 if __name__ == '__main__':
-    urls = extract_urls('data/video_urls.txt')
-    print(urls)
+    urls = extract_urls()
 
     Path('data/video_output').mkdir(parents=True, exist_ok=True)
     serial_download(urls)
