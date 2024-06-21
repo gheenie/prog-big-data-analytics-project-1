4177a000e98948242706ba0a608537108190bb9f
infoomics <sbx_94@hotmail.com>
2024-06-21 13:46:30 +0100

    Switch path to os

--- src/index.py
+++ src/index.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+
 from task_2 import extract_urls
 from task_3 import serial_download, parallel_download
 
@@ -6,6 +8,10 @@ from task_3 import serial_download, parallel_download
 if __name__ == '__main__':
     urls = extract_urls()
 
-    Path('data/video_output').mkdir(parents=True, exist_ok=True)
+    output_folder = "data"
+    output_folder_2 = "video_output"
+    output_path = os.path.join(output_folder, output_folder_2)
+    Path(output_path).mkdir(parents=True, exist_ok=True)
+
     serial_download(urls)
     parallel_download(urls)
