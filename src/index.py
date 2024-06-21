c0b6b6567c6e70eddb20ca2f696cb6e6cbee40eb
infoomics <sbx_94@hotmail.com>
2024-06-21 18:00:38 +0100

    Move input path  to index for SOLID

--- src/index.py
+++ src/index.py
@@ -3,15 +3,20 @@ from pathlib import Path
 
 from task_2 import extract_urls
 from task_3 import serial_download, parallel_download
+from task_5_extract_audio import parallel_extract
 
 
 if __name__ == '__main__':
-    urls = extract_urls()
+    input_folder = "data"
+    input_filename = "video_urls.txt"
+    input_path = os.path.join(input_folder, input_filename)
+
+    urls = extract_urls(input_path)
 
     output_folder = "data"
     output_folder_2 = "video_output"
     output_path = os.path.join(output_folder, output_folder_2)
     Path(output_path).mkdir(parents=True, exist_ok=True)
 
-    serial_download(urls)
-    parallel_download(urls)
+    # serial_download(urls)
+    # parallel_download(urls)
--- src/task_2.py
+++ src/task_2.py
@@ -1,12 +1,8 @@
 import os
 
 
-def extract_urls():
-    output_folder = "data"
-    output_filename = "video_urls.txt"
-    output_path = os.path.join(output_folder, output_filename)
-
-    with open(output_path, 'r') as file:
+def extract_urls(input_path):
+    with open(input_path, 'r') as file:
         urls = file.readlines()
 
     for i, url in enumerate(urls):
