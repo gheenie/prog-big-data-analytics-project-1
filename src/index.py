d804ecb41834a358296ae7a81133cfbaeb048a6f
infoomics <sbx_94@hotmail.com>
2024-06-21 01:33:41 +0100

    Create task folders in data

--- src/index.py
+++ src/index.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 from task_2 import extract_urls
 
 
 if __name__ == '__main__':
-    urls = extract_urls('data/video_urls.txt')
+    urls = extract_urls('data/task_2/video_urls.txt')
     print(urls)
+
+    Path('data/task_3').mkdir(parents=True, exist_ok=True)
