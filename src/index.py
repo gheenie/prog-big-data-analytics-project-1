381ed837d6b7f748b7e76bcc5ce67eeef25140cc
infoomics <sbx_94@hotmail.com>
2024-06-21 18:22:14 +0100

    Complete extract audio

--- src/index.py
+++ src/index.py
@@ -20,3 +20,14 @@ if __name__ == '__main__':
 
     # serial_download(urls)
     # parallel_download(urls)
+
+    # Assume directory contents are all files.
+    video_files = [f for f in os.listdir(output_path)]
+    print(video_files)
+
+    output_folder = "data"
+    output_folder_2 = "extracted_audio"
+    output_path = os.path.join(output_folder, output_folder_2)
+    Path(output_path).mkdir(parents=True, exist_ok=True)
+
+    parallel_extract(video_files)
---
+++ src/task_5_extract_audio.py
@@ -0,0 +1,24 @@
+import os
+from concurrent.futures import ProcessPoolExecutor
+
+import moviepy.editor as mp
+
+
+def extract_audio(video_file):
+    input_folder = "data"
+    input_folder_2 = "video_output"
+    input_path = os.path.join(input_folder, input_folder_2, video_file)
+
+    video = mp.VideoFileClip(input_path)
+
+    output_folder = "data"
+    output_folder_2 = "extracted_audio"
+    output_path = os.path.join(output_folder, output_folder_2, video_file)
+    output_path = f'{output_path}.wav'
+
+    video.audio.write_audiofile(output_path)
+
+
+def parallel_extract(video_files):
+    with ProcessPoolExecutor() as executor:
+        executor.map(extract_audio, video_files)
